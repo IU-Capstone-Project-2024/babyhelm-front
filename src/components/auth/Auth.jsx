e431d47e9207f643515088fd9b174d1c5f14732b
forygg <e.spiridonov@innopolis.university>
2024-06-21 23:09:24 +0300

    feat: added email confirm window, added functional input fields

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import logo from "../../assets/logo.svg"
+import mail from "../../assets/mail 1.png"
 import "./auth.css"
 const Auth = ( { toggleAuthModal } ) => {
 
@@ -19,13 +20,31 @@ const Auth = ( { toggleAuthModal } ) => {
     const [action, setAction] = useState('');
 
     const signUpLink = () => {
-        setAction(' active');
+        setAction(' signup');
+    }
+
+    const submitLink = () => {
+        setAction(' email');
     }
 
     const logInLink = () => {
         setAction('');
     }
     
+    const [account, setAccount] = useState({
+        name: "",
+        surname: "",
+        email: "",
+        password: "",
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setAccount((prevAccount) => ({
+          ...prevAccount,
+          [name]: value
+        }));
+      };
     
     return (
         <div className='babyhelm__auth'>
@@ -38,11 +57,11 @@ const Auth = ( { toggleAuthModal } ) => {
                         <div className='babyhelm__inputs'>
                             <div className='babyhelm__input'>
                                 <p>Email</p>
-                                <input type="email"></input>
+                                <input type="email" name="email" onChange={handleChange}/>
                             </div>
                             <div className='babyhelm__input'>
                                 <p>Password</p>
-                                <input type="password"></input>
+                                <input type="password" name="password" onChange={handleChange}/>
                             </div>
                             <div className="babyhelm__forgot">
                                 <a href="/">Forgot password</a> 
@@ -67,27 +86,27 @@ const Auth = ( { toggleAuthModal } ) => {
                         <div className='babyhelm__inputs'>
                             <div className='babyhelm__input'>
                                 <p>Name</p>
-                                <input type="text"></input>
+                                <input type="text" name="name" value={account.name} onChange={handleChange}/>
                             </div>
                             <div className='babyhelm__input'>
                                 <p>Surname</p>
-                                <input type="text"></input>
+                                <input type="text" name="surname" value={account.surname} onChange={handleChange}/>
                             </div>
                             <div className='babyhelm__input'>
                                 <p>Email</p>
-                                <input type="email"></input>
+                                <input type="email" name="email" value={account.email} onChange={handleChange}/>
                             </div>
                             <div className='babyhelm__input'>
                                 <p>Password</p>
-                                <input type="password"></input>
+                                <input type="password" name="password" value={account.password} onChange={handleChange}/>
                             </div>
                             <div className='babyhelm__input'>
                                 <p>Confirm password</p>
-                                <input type="password"></input>
+                                <input type="password"/>
                             </div>
                         </div>
                         <div className="babyhelm__auth-button">
-                            <button type='submit'>
+                            <button type='submit' onClick={submitLink}>
                                 Sign Up
                             </button>
                         </div>
@@ -95,6 +114,18 @@ const Auth = ( { toggleAuthModal } ) => {
                             <p>Already have an account? <a href="#" onClick={logInLink}>Log In</a></p>
                         </div>
                     </form>
+                </div>
+
+                <div className='babyhelm__auth_email-confirm'>
+                    <div className='babyhelm__auth_header'>
+                        <img src={logo} alt="logo"/>
+                        <h1>Verify your email</h1>
+                    </div>
+                    <p>
+                        {`We’ve sent an email to ${account.email} to verify your email address and activate your account.`}
+                    </p>
+                    <img src={mail} alt="mail"/>
+                    <p><a href="#">Click here</a> if you did not receive an email</p>
                 </div>
             </div>
             
